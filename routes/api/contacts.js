7d020cbba05177d486639f17240006311d1297fd
Vladimirs <98471773+Golotniks94@users.noreply.github.com>
2023-05-25 11:50:31 +0300

    All fix

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,30 +1,30 @@
-const express = require("express");
+const express = require('express')
 
-const router = express.Router();
-const ctrl = require("../../controllers/contacts");
-const { validateBody, isValidId } = require("../../middlewares");
-const { validationContact, validationFavorite } = require("../../models");
+const router = express.Router()
+const ctrl = require('../../controllers/contact-controller')
+const { validateBody, isValidId } = require('../../middlewares')
+const { validationContact, validationFavorite } = require('../../models')
 
-router.get("", ctrl.getAll);
+router.get('', ctrl.getAll)
 
-router.get("/:contactId", isValidId, ctrl.getById);
+router.get('/:contactId', isValidId, ctrl.getById)
 
-router.post("", validateBody(validationContact), ctrl.addContact);
+router.post('', validateBody(validationContact), ctrl.addContact)
 
-router.delete("/:contactId", ctrl.removeContact);
+router.delete('/:contactId', ctrl.removeContact)
 
 router.put(
-  "/:contactId",
+  '/:contactId',
   isValidId,
   validateBody(validationContact),
   ctrl.updateContact
-);
+)
 
 router.patch(
-  "/:contactId/favorite",
+  '/:contactId/favorite',
   isValidId,
   validateBody(validationFavorite),
   ctrl.updateStatusContact
-);
+)
 
-module.exports = router;
+module.exports = router

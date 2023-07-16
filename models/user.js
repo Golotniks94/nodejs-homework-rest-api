e0af1d6ab98e6aca63cd7ca4b1f201b77a8a7206
Vladimirs <98471773+Golotniks94@users.noreply.github.com>
2023-07-16 18:30:28 +0300

    Fix

--- models/user.js
+++ models/user.js
@@ -1,38 +1,12 @@
-const { Schema, model } = require("mongoose");
-const Joi = require("joi");
+const { Schema, model } = require('mongoose')
+const { handleMongooseError, patterns } = require('../helpers')
+const {
+  validationRegistrationUser,
+  validationLoginUser,
+  validationSubscription,
+} = require('../userValidation')
 
-const { handleMongooseError, patterns } = require("../helpers");
-
-const SUBSCRIPTION_TYPES = ["starter", "pro", "business"];
-
-// registration validation user
-const validationRegistrationUser = Joi.object({
-  password: Joi.string().required(),
-  email: Joi.string()
-    .email({ minDomainSegments: 2 })
-    .pattern(patterns.emailPattern)
-    .messages({
-      "string.pattern.base":
-        "Invalid email. Please provide a valid email address",
-    })
-    .required(),
-  subscription: Joi.string().valid(...SUBSCRIPTION_TYPES),
-});
-
-// login validation user
-const validationLoginUser = Joi.object({
-  password: Joi.string().required(),
-  email: Joi.string()
-    .email({ minDomainSegments: 2 })
-    .pattern(patterns.emailPattern)
-    .required(),
-  subscription: Joi.string().valid(...SUBSCRIPTION_TYPES),
-});
-
-// validation subscription
-const validationSubscription = Joi.object({
-  subscription: Joi.string().valid(...SUBSCRIPTION_TYPES),
-});
+const SUBSCRIPTION_TYPES = ['starter', 'pro', 'business']
 
 // mongoose Schema
 const userSchema = new Schema(
@@ -46,7 +20,7 @@ const userSchema = new Schema(
             `Invalid password. Must be at least 6 characters. Got ${props.value.length}`,
         },
       ],
-      required: [true, "The password is required. Set it for user"],
+      required: [true, 'The password is required. Set it for user'],
     },
     email: {
       type: String,
@@ -54,36 +28,36 @@ const userSchema = new Schema(
       match: patterns.emailPattern,
       required: [
         true,
-        "The email is required. Please provide an email address for user",
+        'The email is required. Please provide an email address for user',
       ],
     },
     subscription: {
       type: String,
-      default: "starter",
+      default: 'starter',
       validate: {
         validator: function (v) {
-          return SUBSCRIPTION_TYPES.includes(v);
+          return SUBSCRIPTION_TYPES.includes(v)
         },
         message: (props) =>
           `${
             props.value
           } is not a valid subscription type. Must be: ${SUBSCRIPTION_TYPES.join(
-            ", or "
+            ', or '
           )}`,
       },
     },
-    token: { type: String, default: "" },
+    token: { type: String, default: '' },
     avatarURL: String,
   },
   { versionKey: false, timestamps: true }
-);
+)
 
-userSchema.post("save", handleMongooseError);
-const User = model("user", userSchema);
+userSchema.post('save', handleMongooseError)
+const User = model('user', userSchema)
 
 module.exports = {
   User,
   validationRegistrationUser,
   validationLoginUser,
   validationSubscription,
-};
+}
